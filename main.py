5c8ba0c52ee34d4c4f3422565c25714ab7d3fe29
Ravinder Kadiyan <ravinder.kadiyan@gmail.com>
2025-08-26 12:43:27 +0530

    add left and right paddle, ball and bouncing functionality

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,28 +10,26 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350,0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle(x_position=380)
+l_paddle = Paddle(x_position=-380)
+ball = Ball()
 screen.listen()
 
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()
+    time.sleep(0.1)
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 screen.exitonclick()
